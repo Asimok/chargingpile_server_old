6712b752bd446fabf59d49416ad9963eaebdf9cf
AsimoK <1962976634@qq.com>
2020-07-28 10:52:39 +0800

    在自己服务器运行

--- chargingpile/src/main/java/com/chargingpile/controller/Login.java
+++ chargingpile/src/main/java/com/chargingpile/controller/Login.java
@@ -5,6 +5,8 @@ import com.chargingpile.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +15,9 @@ public class Login {
     private UserService userService;
 
     @PostMapping("/login")
-    public int login(User user){
+    public String login(@RequestBody User user){
         System.out.println(user);
         userService.saveUserInfo(user);
-        return 1;
+        return "success";
     }
 }
